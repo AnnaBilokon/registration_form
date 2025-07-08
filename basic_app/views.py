79d7b02424a36a5a17de048718adad2a76ea6116
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-07-08 14:08:22 +0200

    add login and logout functionality

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from .forms import UserForm, ProfileForm
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'basic_app/index.djhtml')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('basic_app:index'))
+
+
+def special(request):
+    return HttpResponse('You are logged in!')
 
 
 def register(request):
@@ -38,3 +53,25 @@ def register(request):
         'profile_form': profile_form,
         'registered': False
     })
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('basic_app:index'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print('Someone try to login and failed.')
+            print(f'Username: {username} and password: {password}')
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'basic_app/user_login.djhtml', {})
