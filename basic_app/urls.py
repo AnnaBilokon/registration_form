4675d27c25594603bfe77ecb96f6da6d187a59c6
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-07-08 14:07:59 +0200

    add new paths

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -5,5 +5,9 @@ app_name = 'basic_app'
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='logout'),
+
 ]
